d12d225d4681083e0525ad966061d46819e2b17f
emmanuelmarllon <marlonebr339@gmail.com>
2025-12-21 12:55:33 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex w-screen h-screen justify-center items-center">
+      <h1 className="text-4xl font-bold">Welcome to Doutor Agenda</h1>
+    </div>
+  );
+}
