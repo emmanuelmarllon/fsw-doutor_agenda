a63424474f93c705466769c35419fb0d15be78a6
emmanuelmarllon <marlonebr339@gmail.com>
2026-01-02 00:03:56 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex w-screen h-screen justify-center items-center">
-      <h1 className="text-4xl font-bold">Welcome to Doutor Agenda</h1>
+    <div className="dark flex h-screen w-screen flex-col items-center justify-center">
+      <div>
+        <h1 className="text-4xl font-bold">Welcome to Doutor Agenda</h1>
+      </div>
+      <Button className="dark ml-4">Get Started</Button>
     </div>
   );
 }
