81ef39061fa9b1d45a4460f639974297f5e07290
emmanuelmarllon <marlonebr339@gmail.com>
2026-01-02 15:43:44 -0300

    feat: add clinic form page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
-import { Button } from "@/components/ui/button";
+import { headers } from "next/headers";
+import Link from "next/link";
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
+
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  const isLogged = Boolean(session?.user);
+
   return (
-    <div className="dark flex h-screen w-screen flex-col items-center justify-center">
-      <div>
-        <h1 className="text-4xl font-bold">Welcome to Doutor Agenda</h1>
-      </div>
-      <Button className="dark ml-4">Get Started</Button>
+    <div className="dark flex h-screen w-screen flex-col items-center justify-center gap-6">
+      <h1 className="text-4xl font-bold">
+        Bem-vindo ao Doutor Agenda
+      </h1>
+
+      <Button asChild className="text-white">
+        <Link href={isLogged ? "/dashboard" : "/authentication"}>
+          {isLogged ? "Ir para o dashboard" : "Fazer login"}
+        </Link>
+      </Button>
     </div>
   );
 }
